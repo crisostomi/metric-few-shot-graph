Fix run collisions, substitute softmax with log_softmax, reset optimizers

src/fs_grl/modules/mlp.py
@@ -39,4 +39,4 @@ class MLP(nn.Module):
         elif non_linearity == "tanh":
             return nn.Tanh()
         else:
-            raise NotImplementedError(f"No such activaction {non_linearity}")
+            raise NotImplementedError(f"No such activation {non_linearity}")

src/fs_grl/pl_modules/transfer_learning.py
@@ -18,16 +18,18 @@ class TransferLearningBaseline(MyLightningModule, ABC):
         super().__init__()
 
     def validation_epoch_end(self, outputs: EPOCH_OUTPUT) -> None:
-        self.log_metrics(split="val", on_step=False, on_epoch=True)
+        self.log_metrics(split="val", on_step=False, on_epoch=True, cm_reset=True)
 
-    def log_metrics(self, split: str, on_step: bool, on_epoch: bool):
+    def log_metrics(self, split: str, on_step: bool, on_epoch: bool, cm_reset: bool):
         to_log = {}
 
         for metric_name, metric in getattr(self, f"{split}_metrics").items():
             if "none" in metric_name:
                 self.handle_no_average_metric(metric_name, metric, to_log)
             elif "cm" in metric_name:
-                self.handle_confusion_matrix(metric, split=split)
+                self.handle_confusion_matrix(metric_name, metric)
+                if cm_reset:
+                    metric.reset()
             else:
                 to_log[metric_name] = metric
 
@@ -43,12 +45,11 @@ class TransferLearningBaseline(MyLightningModule, ABC):
             to_log[f"{metric_name}/{label}"] = score
         metric.reset()
 
-    def handle_confusion_matrix(self, metric, split: str):
+    def handle_confusion_matrix(self, cm_name, metric):
         fig: go.Figure = self.plot_cm(cm=metric)
         wandb.log(
-            data={f"{split}/confusion_matrix": fig},
+            data={cm_name: fig},
         )
-        metric.reset()
 
     def plot_cm(self, cm: ConfusionMatrix) -> go.Figure:
         z: np.ndarray = cm.compute().cpu().numpy()

src/fs_grl/pl_modules/transfer_learning_target.py
@@ -1,11 +1,16 @@
+import itertools
 import logging
-from typing import Any, Dict, Mapping, Optional
+from typing import Any, Dict, Mapping, Optional, Sequence, Tuple, Union
 
+import hydra
+import pytorch_lightning
 import torch
 import torchmetrics
 from pytorch_lightning.utilities.types import STEP_OUTPUT
 from torch import nn
-from torchmetrics import FBetaScore
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LambdaLR
+from torchmetrics import Accuracy, FBetaScore
 
 from fs_grl.data.datamodule import MetaData
 from fs_grl.data.episode import EpisodeBatch
@@ -39,26 +44,24 @@ class TransferLearningTarget(TransferLearningBaseline):
         self.classes = metadata.classes_split["novel"]
         self.log_prefix = "meta-testing"
 
-        reductions = ["micro", "weighted", "macro"]  # TODO: add None
-        metrics = ["F1", "acc"]
+        reductions = ["micro", "weighted", "macro", "none"]
+        metrics = (("F1", FBetaScore), ("acc", Accuracy))
 
         self.train_metrics = nn.ModuleDict(
             {
-                f"{self.log_prefix}/train/{metric}/{reduction}": FBetaScore(
+                f"{self.log_prefix}/train/{metric_name}/{reduction}": metric(
                     num_classes=len(self.classes), average=reduction
                 )
-                for reduction in reductions
-                for metric in metrics
+                for reduction, (metric_name, metric) in itertools.product(reductions, metrics)
             }
         )
 
         self.test_metrics = nn.ModuleDict(
             {
-                f"{self.log_prefix}/test/{metric}/{reduction}": FBetaScore(
+                f"{self.log_prefix}/test/{metric_name}/{reduction}": metric(
                     num_classes=len(self.classes), average=reduction
                 )
-                for reduction in reductions
-                for metric in metrics
+                for reduction, (metric_name, metric) in itertools.product(reductions, metrics)
             }
         )
 
@@ -116,21 +119,57 @@ class TransferLearningTarget(TransferLearningBaseline):
         self.freeze_embedder()
 
     def on_train_batch_end(self, outputs: STEP_OUTPUT, batch: Any, batch_idx: int, unused: Optional[int] = 0) -> None:
+        # It's testing time!
         logits = self(batch.queries)["logits"]
 
-        class_probs = torch.softmax(logits, dim=-1)
+        class_probs = torch.log_softmax(logits, dim=-1)
         preds = torch.argmax(class_probs, dim=-1)
 
         for metric in self.test_metrics.values():
             metric(preds=preds, target=batch.queries.y)
 
-        self.log_metrics(split="test", on_step=True, on_epoch=True)
+        self.log_metrics(split="test", on_step=True, on_epoch=True, cm_reset=False)
 
     def reset_fine_tuning(self):
         self.load_state_dict(torch.load(self.initial_state_path))
+        self.trainer: pytorch_lightning.Trainer
+        (
+            self.trainer.optimizers,
+            self.trainer.lr_schedulers,
+            self.trainer.optimizer_frequencies,
+        ) = self.trainer.init_optimizers(model=None)
 
     def freeze_embedder(self):
         """ """
         self.embedder.eval()
-        for param in self.embedder.parameters():
-            param.requires_grad = False
+        self.embedder.requires_grad_(False)
+
+    def configure_optimizers(
+        self,
+    ) -> Union[Optimizer, Tuple[Sequence[Optimizer], Sequence[Any]]]:
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+        Return:
+            Any of these 6 options.
+            - Single optimizer.
+            - List or Tuple - List of optimizers.
+            - Two lists - The first list has multiple optimizers, the second a list of LR schedulers (or lr_dict).
+            - Dictionary, with an 'optimizer' key, and (optionally) a 'lr_scheduler'
+              key whose value is a single LR scheduler or lr_dict.
+            - Tuple of dictionaries as described, with an optional 'frequency' key.
+            - None - Fit will run without any optimizer.
+        """
+        opt = hydra.utils.instantiate(self.hparams.optimizer, params=self.parameters(), _convert_="partial")
+
+        schedulers = []
+        if "lr_scheduler" not in self.hparams:
+            lr_scheduler_config = {
+                "scheduler": LambdaLR(optimizer=opt, lr_lambda=lambda _: 1),
+                "name": f"meta-testing/lr-{opt.__class__.__name__}",
+            }
+
+            schedulers.append(lr_scheduler_config)
+        else:
+            schedulers.append(hydra.utils.instantiate(self.hparams.lr_scheduler, optimizer=opt))
+
+        return [opt], schedulers