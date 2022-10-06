7b339a50bd6df55413fa7df152772d063efac711
santonelli7 <simone.antonelli97@gmail.com>
2022-10-06 16:03:59 +0200

    minor fixes

--- src/fs_grl/custom_pipelines/graph_kernel/graph_kernel_module.py
+++ src/fs_grl/custom_pipelines/graph_kernel/graph_kernel_module.py
@@ -1,5 +1,4 @@
 import logging
-import time
 from typing import Any, Mapping, Optional
 
 import torch
@@ -35,37 +34,19 @@ class GraphKernel(BaseModule):
         :return similarities, tensor ~ (B*(N*Q)*N) containing for each episode the similarity
                 between each of the N*Q queries and the N label prototypes
         """
-        ts = time.time()
-        kernel = instantiate(self.hparams.graph_kernel, _recursive_=False)
-        print(f"Instantiate kernel: {time.time() - ts}")
+        kernel = instantiate(self.hparams.kernel_method, _recursive_=False)
         clf = instantiate(self.hparams.classifier, _recursive_=False)
-        print(f"Instantiate classifier: {time.time() - ts}")
 
         supports = batch_to_grakel(batch.supports, self.hparams.node_labels_tag)
         supports_y = batch.supports.y.numpy()
         queries = batch_to_grakel(batch.queries, self.hparams.node_labels_tag)
-        print(f"Dataloader to grakel: {time.time() - ts}")
 
         K_sup = kernel.fit_transform(supports)
-        print(f"Fit kernel: {time.time() - ts}")
-
-        # unique_supports_y, _ = np.unique(supports_y, return_index=True)
-
-        # prototypes = []
-        # for cls in unique_supports_y:
-        #     indices = supports_y == cls
-        #     prototype = K_sup[indices].mean(axis=0)
-        #     prototypes.append(prototype)
-
-        # prototypes = np.array(prototypes)
 
         clf.fit(K_sup, supports_y)
-        print(f"Fit classifier: {time.time() - ts}")
 
         K_qu = kernel.transform(queries)
-        print(f"Kernel queries: {time.time() - ts}")
         y_pred = clf.predict(K_qu)
-        print(f"Query predictions: {time.time() - ts}")
 
         return torch.from_numpy(y_pred)
 
